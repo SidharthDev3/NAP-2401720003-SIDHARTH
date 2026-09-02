f611e220886fb90f65c241b51045414c8cfad8ea
SIDHARTH <codefather0003@gmail.com>
2026-09-02 11:27:18 +0530

    Arrays in GO started

---
+++ GO/arrays/array.go
@@ -0,0 +1,16 @@
+package arrays
+
+import "fmt"
+
+func Array() {
+	var marks [5]int
+
+	for i := 0; i < len(marks); i++ {
+		fmt.Printf("Enter marks of subject %d :", i+1)
+		fmt.Scan(&marks[i])
+
+	}
+	for i := 0; i < len(marks); i++ {
+		fmt.Printf("Mark of subject %d %d \n", i+1, marks[i])
+	}
+}
---
+++ GO/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "newageprogramming/arrays"
+
+func main() {
+	arrays.Array()
+
+}
